1da98035da923aa72cb4dfebc1c2c3346620eb48
ovsds <ovsds@yandex-team.ru>
2024-10-04 14:56:33 +0200

    feat: add scripts

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,10 +8,14 @@ def remove_folder(folder_path: str) -> None:
     shutil.rmtree(folder)
 
 
-def remove_file(file_path: str) -> None:
+def remove_file(file_path: str, missing_ok: bool = True) -> None:
     file = pathlib.Path(file_path)
-    file.unlink()
+    file.unlink(missing_ok=missing_ok)
 
 
 if __name__ == "__main__":
-    pass
+    if "{{ cookiecutter.scripts }}" != "true":
+        remove_folder(".scripts")
+
+    # Remove files from older versions
+    remove_file(".husky/.huskyrc")
